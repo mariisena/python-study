f63f12abd0d39381b972b7e7d82be89b94590f16
Mariana Sena <marianasena@sempreceub.com>
2025-01-15 18:31:28 -0300

    primeiro commit 2025

--- Codédex/The Legend of Python/Course 1 - Python/08. Modules/wiki.py
+++ Codédex/The Legend of Python/Course 1 - Python/08. Modules/wiki.py
@@ -1,13 +1,11 @@
-import wiki
+import wikipedia
 
-# Ask the user for a search term
 search_term = input("Enter a phrase to search on Wikipedia: ")
 
-# Fetch a summary from Wikipedia
 try:
-    summary = wiki.summary(search_term, sentences=3)
+    summary = wikipedia.summary(search_term, sentences=3)
     print(f"\nSummary for '{search_term}':\n{summary}")
-except wiki.exceptions.DisambiguationError as e:
+except wikipedia.exceptions.DisambiguationError as e:
     print(f"\nThe term '{search_term}' is ambiguous. Here are some suggestions:\n{e.options}")
-except wiki.exceptions.PageError:
+except wikipedia.exceptions.PageError:
     print(f"\nThe page for '{search_term}' does not exist. Please try another search term.")
